50a7b1954647eacad602f0a093240d1f34805ac3
BlockProgram <blockprogram@protonmail.com>
2020-02-02 00:37:06 +0100

    add date alert and count program

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-console.log("Hello, world! \n consoleman"); 
+const y = "you da best";
+const x = ""; 
+console.log(y.includes("you da best my man")); 
+
+const lorem = "abcdefghijklmnopqrstuvwxyz"; 
+for (let i = 0; i < lorem.length; i++) { console.log(lorem[i]) }
+
+let b = [55, 20, "$$", lorem.includes("abce")];
