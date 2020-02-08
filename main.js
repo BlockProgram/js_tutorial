c67385599e4c03a38277af967b48828cd562d0f0
BlockProgram <blockprogram@protonmail.com>
2020-02-08 23:56:54 +0100

    add form for interactive html palindrome testing

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let Phrase = require("mhartl-palindrome"); 
 
-function palindromeTester() { 
-    let string = prompt("Please enter a string for palindrome testing"); 
-    let phrase = new Phrase(string); 
+function palindromeTester(event) {
+    event.preventDefault(); 
     
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
+  
     if (phrase.palindrome()) {
-        return alert(`"${phrase.content}" is a palindrome!`); 
-        } else {    
-        return alert(`${phrase.content}" is not a palindrome`); 
-        }      
- } 
-
- let button = document.querySelector("#palindromeTester"); 
-button.addEventListener("click", function() {  
-    palindromeTester(); 
-});
+      palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+    } else {
+      palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    let button = document.querySelector("#palindromeTester");
+    button.addEventListener("submit", function() {
+      palindromeTester(event);
+    });
+  });
+  
